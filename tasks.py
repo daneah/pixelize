ffbc3333d0cfad1442aff572e3b33a54a1b4f2b7
Dane Hillard <github@danehillard.com>
2019-08-23 12:03:19 -0400

    Convert deprecated scipy approach to Pillow.

--- tasks.py
+++ tasks.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 import numpy
-from scipy import misc
+from PIL import Image
 
 from invoke import task
 
@@ -24,12 +24,12 @@ def pixelize(ctx, infile, output_height=0, output_width=0, output_size=512, samp
     if not os.path.exists(infile):
         sys.exit('File {} does not exist!'.format(infile))
 
-    image = misc.imread(infile)
+    image = Image.open(infile)
 
     try:
-        orig_height, orig_width, num_channels = image.shape
+        orig_height, orig_width, num_channels = image.size
     except:
-        orig_height, orig_width = image.shape
+        orig_height, orig_width = image.size
 
     if output_height and not output_width:
         output_width = int(orig_width * (orig_height / output_height))
@@ -38,15 +38,15 @@ def pixelize(ctx, infile, output_height=0, output_width=0, output_size=512, samp
     elif not any([output_height, output_width]):
         output_height = output_width = output_size
 
-    image = misc.imresize(image, (output_height // sampling_factor or 1, output_width // sampling_factor or 1), 'lanczos')
-    image = numpy.rot90(image, k=3)
-    image = numpy.sort(image, axis=0)
-    image = numpy.rot90(image, k=2)
-    image = misc.imresize(image, (output_height, output_width), 'nearest')
+    image = image.resize((output_width // sampling_factor or 1, output_height // sampling_factor or 1), Image.LANCZOS)
+    image = image.rotate(90 * 3)
+    image = Image.fromarray(numpy.sort(image, axis=0))
+    image = image.rotate(90 * 2)
+    image = image.resize((output_width, output_height), Image.NEAREST)
 
     filename_parts = os.path.basename(infile).split('.')
     filename_parts.insert(-1, 'processed')
 
     processed_image_filename = os.path.join(os.path.dirname(infile), '.'.join(filename_parts))
 
-    misc.imsave(processed_image_filename, image)
+    image.save(open(processed_image_filename, 'w'))
